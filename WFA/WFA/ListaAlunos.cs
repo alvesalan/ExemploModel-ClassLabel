4649a5da531333a69e0e5119babfcafdd691ebcd
alvesalan <38699636+alvesalan@users.noreply.github.com>
2018-06-05 17:28:03 -0300

    Exemplo01+ListaAluno

---
+++ WFA/WFA/ListaAlunos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFA
+{
+    public partial class ListaAlunos : Form
+    {
+        public ListaAlunos()
+        {
+            InitializeComponent();
+        }
+    }
+}
